c8753762e54ec094d20ec8ffdd3663661b5b5035
aniaan <hi@aniaan.dev>
2024-10-28 01:07:30 +0800

    update

--- init.zsh
+++ init.zsh
@@ -1,12 +1,7 @@
-if (( ! ${+commands[rustup]} && ! ${+commands[cargo]} )); then
+if (( ! ${+commands[rustup]} || ! ${+commands[cargo]} )); then
   return 1
 fi
-
-# fpath=("${0:h}/functions" $fpath)
 
 if [[ ! -f "${0:h}/functions/_rustup" ]]; then
   rustup completions zsh > "${0:h}/functions/_rustup"
 fi
-autoload -Uz _rustup
-autoload -Uz _cargo
-
